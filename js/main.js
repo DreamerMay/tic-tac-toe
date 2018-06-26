6e7d71fec3da457b3b37dc54f2e72051f4064981
Sherine <sherinemay79@gmail.com>
2018-06-26 10:47:08 +1000

    completed logi in js

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@
 // When user
 
 const board = [
-  " ", " ", " ",
-  " ", " ", " ",
-  " ", " ", " "
+  "o", "o", "o",
+  "", "", "",
+  "", "", ""
 ];
 
 const xPlayerArray = [];
@@ -47,29 +47,49 @@ const player_turns = function(index) {
   }
 };
 
+// check back later;
 // const ready_to_check = function (xPlayerArray, oPlayerArray) {
-//   if (xplayerArray.lenght >= 3) || (oplayerArray.lenght >= 3) {
-//     // ** check win to win function.
+//   if (xplayerArray.length >= 3 || oplayerArray.length >= 3) {
+//     return check_match();
 //     }
-//     else return;
-//   }
-//
-//
-//
-// const win_rule = function (xplayerArray, oPlayerArray) {
-//   let win1 = [0,1,2];
-//   let win2 = [3,4,5];
-//   let win3 = [6,7,8];
-//   let win4 = [0,3,6];
-//   let win5 = [1,4,7];
-//   let win6 = [2,5,8];
-//   let win7 = [0,4,8];
-//   let win8 = [2,4,6];
-//   if (xPlayerArray.lenght >= 3) {
+//     else {
+//       return false;
+//     }
+//   };
 
-
-
-
-  //
-  //   }
-  // };
+const check_match = function () {
+  // let win1 = [0,1,2];
+  // let win2 = [3,4,5];
+  // let win3 = [6,7,8];
+  // let win4 = [0,3,6];
+  // let win5 = [1,4,7];
+  // let win6 = [2,5,8];
+  // let win7 = [0,4,8];
+  // let win8 = [2,4,6];
+  if (board[0] === board[1] && board[1] === board[2] && board[0] != "") {
+    console.log(`${board[0]} Player win!`);
+  }
+  else if (board[3] === board[4] && board[4] === board[5] && board[3] != "") {
+    console.log(`${board[3]} Player win!`);
+  }
+  else if (board[6] === board[7] && board[7] === board[8] && board[6] != "") {
+    console.log(`${board[6]} Player win!`);
+  }
+  else if (board[0] === board[3] && board[3] === board[6] && board[0] != "") {
+    console.log(`${board[0]} Player win!`);
+  }
+  else if (board[1] === board[4] && board[4] === board[7] && board[1] != "") {
+    console.log(`${board[0]} Player win!`);
+  }
+  else if (board[2] === board[5] && board[5] === board[8] && board[2] != "") {
+    console.log(`${board[2]} Player win!`);
+  }
+  else if (board[0] === board[4] && board[4] === board[8] && board[0] != "") {
+    console.log(`${board[0]} Player win!`);
+  }
+  else if (board[2] === board[4] && board[4] === board[6] && board[2] != " ") {
+    console.log(`${board[2]} Player win!`);
+  } else {
+    console.log("Draw");
+  }
+};
